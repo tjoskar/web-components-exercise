ebe7c50d4b75529d8dba75cb7fee85faa5857823
Tjoskar <mr.karlsson.oskar@gmail.com>
2018-10-14 11:50:52 +0200

    ✨  Add a todo implementation

--- native-todo/todo-app.js
+++ native-todo/todo-app.js
@@ -1,6 +1,81 @@
+const template = document.createElement('template')
+template.innerHTML = `
+    <h1>Todos</h1>
+    <section>
+        <todo-input></todo-input>
+        <ul></ul>
+    </section>
+`
+
 export class TodoApp extends HTMLElement {
   constructor() {
     super()
     console.log('🚀')
+
+    this.items = [
+      {
+        uid: 1,
+        name: 'Buy a cat',
+        checked: true
+      },
+      {
+        uid: 2,
+        name: 'Buy a dog',
+        checked: false
+      }
+    ]
+    this.root = this.attachShadow({ mode: 'open' })
+  }
+
+  connectedCallback() {
+    this.root.appendChild(template.content.cloneNode(true))
+    this.inputNode = this.root.querySelector('todo-input')
+    this.todoListNode = this.root.querySelector('ul')
+    this.inputNode.addEventListener('onNewTodo', e => this.addItem(e))
+    this.render()
+  }
+
+  addItem({ detail: { name } }) {
+    const newItem = {
+      uid: Math.random(),
+      name,
+      checked: false
+    }
+    this.items.push(newItem)
+    this.render()
+  }
+
+  changeChecked({ detail: { uid, checked } }) {
+    this.items = this.items.map(item => {
+      if (item.uid === uid) {
+        return { ...item, checked }
+      }
+      return item
+    })
+    this.render()
+  }
+
+  render() {
+    const todoItemNodes = Array.from(this.todoListNode.childNodes)
+    this.items.forEach(item => {
+      const itemNode = todoItemNodes.find(i => Number(i.uid) === item.uid)
+
+      if (!itemNode) {
+        const newItemNode = document.createElement('todo-item')
+        newItemNode.setAttribute('name', item.name)
+        newItemNode.setAttribute('uid', item.uid)
+        if (item.checked) {
+          newItemNode.setAttribute('checked', item.checked)
+        }
+        newItemNode.addEventListener('onCheckedChange', e => this.changeChecked(e))
+        this.todoListNode.appendChild(newItemNode)
+      } else if (item.checked !== Boolean(itemNode.checked)) {
+        if (item.checked) {
+          itemNode.setAttribute('checked', true)
+        } else {
+          itemNode.removeAttribute('checked')
+        }
+      }
+    })
   }
 }
--- native-todo/todo-item.js
+++ native-todo/todo-item.js
@@ -1,5 +1,55 @@
+const template = document.createElement('template')
+template.innerHTML = `
+  <li>
+    <input type="checkbox">
+    <label></label>
+  </li>
+`
+
 export class TodoItem extends HTMLElement {
   constructor() {
     super()
+    console.log('💽 A new item was created!')
+    this.root = this.attachShadow({ mode: 'open' })
+  }
+
+  static get observedAttributes() {
+    return ['checked']
+  }
+
+  connectedCallback() {
+    this.root.appendChild(template.content.cloneNode(true))
+    this.nameNode = this.root.querySelector('label')
+    this.checkboxNode = this.root.querySelector('input')
+    this.uid = Number(this.getAttribute('uid'))
+    this.name = this.getAttribute('name')
+    this.checkboxNode.addEventListener('click', () => {
+      this.dispatchEvent(
+        new CustomEvent('onCheckedChange', {
+          detail: { uid: this.uid, checked: !this.checked }
+        })
+      )
+    })
+    this.render()
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name == 'checked') {
+      this.checked = Boolean(newValue)
+    }
+    if (this.nameNode) {
+      this.render()
+    }
+  }
+
+  render() {
+    this.nameNode.textContent = this.name
+    if (this.checked) {
+      this.nameNode.classList.add('checked')
+      this.checkboxNode.checked = true
+    } else {
+      this.nameNode.classList.remove('checked')
+      this.checkboxNode.checked = false
+    }
   }
 }
